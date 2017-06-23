1e27a1595f6f8b58b4af156dbfe5459e17cd7487
Ich <you@example.com>
2017-06-23 10:40:16 +0200

    error checks

--- make_booklet.sh
+++ make_booklet.sh
@@ -5,70 +5,71 @@ clear
 #
 ###############################################################################
 #
-echo " "
-echo " "
+TEX_FILE="basic_english"
+#
 echo "########################################################################"
-echo " "
 echo "start $0"
 echo " "
-echo "########################################################################"
-echo " "
-
-echo "Erstelle dvi-Datei"
-echo "------------------------------------------------------------------"
-latex basic_english.tex > /dev/null
-echo " "
-
-echo "Erstelle Inhaltsverzeichnis"
-echo "------------------------------------------------------------------"
-latex basic_english.tex > /dev/null
-ls -l basic_english.dvi 
-echo " "
-
-echo "Erstelle pdf-Datei"
-echo "------------------------------------------------------------------"
-pdflatex basic_english.tex > /dev/null
-ls -l basic_english.pdf 
-
-echo "Erstelle ps-Datei"
-echo "------------------------------------------------------------------"
-dvips basic_english.dvi > /dev/null 2> /dev/null
-ls -l basic_english.ps
-echo " "
-
-echo "Erstelle Booklet ps-file" 
-echo "------------------------------------------------------------------"
-pstops 4:-3L@.7\(21cm,0\)+0L@.7\(21cm,14.85cm\),1L@.7\(21cm,0\)+-2L@.7\(21cm,14.85cm\) basic_english.ps basic_english-booklet.ps > /dev/null 2> /dev/null
-
-echo " "
-echo "Erstelle Booklet pdf-file" 
-echo "------------------------------------------------------------------"
-ps2pdf basic_english-booklet.ps
-ls -l basic_english-booklet.*
-echo " "
-
-echo "Make html files"
-echo "------------------------------------------------------------------"
-echo "Configuration file: .latex2html-init"
-latex2html basic_english.tex > /dev/null 2> /dev/null
-echo " "
-ls basic_english
-echo " "
-
-echo "Create Build directory and copy the build files in this directory"
+echo "make dvi file"
+latex $TEX_FILE.tex > /dev/null 
+if [ ! -f $TEX_FILE.dvi ]; then
+	echo "ERROR"
+	exit 1
+fi
+echo "make table of contents"
+latex $TEX_FILE.tex > /dev/null
+echo "make pdf file"
+pdflatex $TEX_FILE.tex > /dev/null
+if [ ! -f $TEX_FILE.pdf ]; then
+	echo "ERROR"
+	exit 1
+fi
+echo "make ps file"
+dvips $TEX_FILE.dvi > /dev/null 2> /dev/null
+if [ ! -f $TEX_FILE.ps ]; then
+	echo "ERROR"
+	exit 1
+fi
+echo "make booklet ps file" 
+pstops 4:-3L@.7\(21cm,0\)+0L@.7\(21cm,14.85cm\),1L@.7\(21cm,0\)+-2L@.7\(21cm,14.85cm\) $TEX_FILE.ps $TEX_FILE-booklet.ps > /dev/null 2> /dev/null
+if [ ! -f $TEX_FILE-booklet.ps ]; then
+	echo "ERROR"
+	exit 1
+fi
+echo "make booklet pdf file" 
+ps2pdf $TEX_FILE-booklet.ps
+if [ ! -f $TEX_FILE-booklet.pdf ]; then
+	echo "ERROR"
+	exit 1
+fi
+echo "make html files"
+latex2html $TEX_FILE.tex > /dev/null 2> /dev/null
+if [ ! -f $TEX_FILE/index.html ]; then
+	echo "ERROR"
+	exit 1
+fi
+echo "Create Build directory and copy the pdf and html files in this directory."
 rm -rf _build
 mkdir -p _build
 mv *.pdf _build
-mv basic_english _build/
-
-
-echo "Delete backup files"
+if [ ! -f _build/$TEX_FILE.pdf ]; then
+	echo "ERROR"
+	exit 1
+fi
+if [ ! -f _build/$TEX_FILE-booklet.pdf ]; then
+	echo "ERROR"
+	exit 1
+fi
+mv $TEX_FILE _build/
+if [ ! -f _build/$TEX_FILE/index.html ]; then
+	echo "ERROR"
+	exit 1
+fi
+echo "make clear"
 rm -f *.synctex.gz
 rm -f *.tmp
 rm -f tmp.*
 rm -f *.swp
-
-echo "Delete core latex/pdflatex auxiliary files"
 rm -f *.aux
 rm -f *.lof
 rm -f *.log
@@ -96,23 +97,15 @@ rm -f *.ilg
 rm -f *.ind
 rm -f *.ist
 echo " "
-
-echo "########################################################################"
-echo " "
 echo "The pdf and html files are in the directory _build."
-ls -l _build
+ls _build
 echo " "
 echo "Printing of the booklet: "
 echo "Print the odd numbers first."
 echo "Put the paper with the script on the top again in the printer."
-
-echo " "
-echo "########################################################################"
 echo " "
 echo "end $0"
-echo " "
 echo "########################################################################"
-echo " "
 echo " "
 ###############################################################################
 # eof
