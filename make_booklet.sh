b2d3df96a88b0ad50127c889432c83283377568a
Ich <you@example.com>
2017-06-23 09:37:49 +0200

    make_booklet.sh with _build directory and cleaning after build

--- make_booklet.sh
+++ make_booklet.sh
@@ -5,6 +5,15 @@ clear
 #
 ###############################################################################
 #
+echo " "
+echo " "
+echo "########################################################################"
+echo " "
+echo "start $0"
+echo " "
+echo "########################################################################"
+echo " "
+
 echo "Erstelle dvi-Datei"
 echo "------------------------------------------------------------------"
 latex basic_english.tex > /dev/null
@@ -13,23 +22,23 @@ echo " "
 echo "Erstelle Inhaltsverzeichnis"
 echo "------------------------------------------------------------------"
 latex basic_english.tex > /dev/null
-ls -l latex basic_english.dvi 
+ls -l basic_english.dvi 
 echo " "
 
 echo "Erstelle pdf-Datei"
 echo "------------------------------------------------------------------"
 pdflatex basic_english.tex > /dev/null
-ls -l latex basic_english.pdf 
+ls -l basic_english.pdf 
 
 echo "Erstelle ps-Datei"
 echo "------------------------------------------------------------------"
-dvips basic_english.dvi > /dev/null
+dvips basic_english.dvi > /dev/null 2> /dev/null
 ls -l basic_english.ps
 echo " "
 
 echo "Erstelle Booklet ps-file" 
 echo "------------------------------------------------------------------"
-pstops 4:-3L@.7\(21cm,0\)+0L@.7\(21cm,14.85cm\),1L@.7\(21cm,0\)+-2L@.7\(21cm,14.85cm\) basic_english.ps basic_english-booklet.ps
+pstops 4:-3L@.7\(21cm,0\)+0L@.7\(21cm,14.85cm\),1L@.7\(21cm,0\)+-2L@.7\(21cm,14.85cm\) basic_english.ps basic_english-booklet.ps > /dev/null 2> /dev/null
 
 echo " "
 echo "Erstelle Booklet pdf-file" 
@@ -41,18 +50,69 @@ echo " "
 echo "Make html files"
 echo "------------------------------------------------------------------"
 echo "Configuration file: .latex2html-init"
-latex2html basic_english.tex > /dev/null
-ls -lrt basic_english
+latex2html basic_english.tex > /dev/null 2> /dev/null
+echo " "
+ls basic_english
 echo " "
 
+echo "Create Build directory and copy the build files in this directory"
+rm -rf _build
+mkdir -p _build
+mv *.pdf _build
+mv basic_english _build/
 
-## acroread basic_english-booklet.pdf &
-## ghostview -landscape basic_english-booklet.ps &
-## kghostview basic_english-booklet.ps &
 
+echo "Delete backup files"
+rm -f *.synctex.gz
+rm -f *.tmp
+rm -f tmp.*
+rm -f *.swp
+
+echo "Delete core latex/pdflatex auxiliary files"
+rm -f *.aux
+rm -f *.lof
+rm -f *.log
+rm -f *.lot
+rm -f *.fls
+rm -f *.out
+rm -f *.toc
+rm -f *.fmt
+rm -f *.fot
+rm -f *.cb
+rm -f *.cb2
+rm -f *.dvi
+rm -f *.ps
+rm -f *.eps
+rm -f *.pdf
+rm -f *.bbl
+rm -f *.blg
+rm -f *.bcf
+rm -f *.blg
+rm -f *-blx.aux
+rm -f *-blx.bib
+rm -f *.run.xml
+rm -f *.idx
+rm -f *.ilg
+rm -f *.ind
+rm -f *.ist
+echo " "
+
+echo "########################################################################"
+echo " "
+echo "The pdf and html files are in the directory _build."
+ls -l _build
+echo " "
+echo "Printing of the booklet: "
 echo "Print the odd numbers first."
 echo "Put the paper with the script on the top again in the printer."
 
-
+echo " "
+echo "########################################################################"
+echo " "
+echo "end $0"
+echo " "
+echo "########################################################################"
+echo " "
+echo " "
 ###############################################################################
 # eof
